58f798f3918857435e7aff5c428a6eaedb393bb2
zhenlee2 <113096519+zhenlee2@users.noreply.github.com>
2023-05-16 16:45:06 +0800

    comper setup

---
+++ storage/framework/views/daef53c2b241baea5ad504f6d8cf7a489df11537.php
@@ -0,0 +1,32 @@
+
+<?php $__env->startSection('title', 'Error 500'); ?>
+
+<?php $__env->startSection('css'); ?>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('style'); ?>
+<?php $__env->stopSection(); ?>
+
+
+<?php $__env->startSection('content'); ?>
+<div class="page-wrapper compact-wrapper" id="pageWrapper">
+  <!-- error-500 start-->
+  <div class="error-wrapper">
+    <div class="container"><img class="img-100" src="<?php echo e(asset('assets/images/other-images/sad.png')); ?>" alt="">
+      <div class="error-heading">
+        <h2 class="headline font-primary">500</h2>
+      </div>
+      <div class="col-md-8 offset-md-2">
+        <p class="sub-content">The page you are attempting to reach is currently not available. This may be because the page does not exist or has been moved.</p>
+      </div>
+      <div><a class="btn btn-primary-gradien btn-lg" href="<?php echo e(route('/')); ?>">BACK TO HOME PAGE</a></div>
+    </div>
+  </div>
+  <!-- error-500 end-->
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('script'); ?>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.errors.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\opc-watch\resources\views/errors/500.blade.php ENDPATH**/ ?>
