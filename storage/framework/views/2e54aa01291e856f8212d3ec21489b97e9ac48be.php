095671c90d0671ca269adac33f2753dcda6972d2
zhenlee2 <113096519+zhenlee2@users.noreply.github.com>
2023-06-06 14:21:20 +0800

    gitignore changes

--- storage/framework/views/2e54aa01291e856f8212d3ec21489b97e9ac48be.php
+++
@@ -1,3 +0,0 @@
-<h1>
-    <p>HOME</p>
-<h1><?php /**PATH C:\laragon\www\cubastarterkit\resources\views/home.blade.php ENDPATH**/ ?>
--- storage/framework/views/f3968fef4b8af8be55b2c81abe60f72672e155cb.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-	  <div class="container-fluid">
-		    <div class="row">
-			      <div class="col-md-12 footer-copyright text-center">
-			        	<p class="mb-0">Copyright <?php echo e(date('Y')); ?> © DSWD FO Caraga Policy Development and Planning Section  </p>
-			      </div>
-		    </div>
-	  </div>
-</footer><?php /**PATH C:\laragon\www\opc-watch\resources\views/layouts/simple/footer.blade.php ENDPATH**/ ?>
